3c63ff8bbc86326efc26666d1c306fe6a7b4488e
poorna2511 <89305345+poorna2511@users.noreply.github.com>
2022-04-17 23:32:53 +0530

    sample code on how to perform training and testing

--- NeuralNetworks_real/NeuralNetworks/main.cpp
+++ NeuralNetworks_real/NeuralNetworks/main.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
-#include<fstream>
 #include<vector>
+
+#include "CNeuralNetwork.h"
 
 using namespace std;
 
 int main()
 {
+	vector<vector<double>> posData, negData;//just for sample we are taking empty vctor
+	//after loading pos and neg data
+
+	vector<int> neuronsCountsInHiddenLayer;
+	neuronsCountsInHiddenLayer.push_back(100);
+	neuronsCountsInHiddenLayer.push_back(100);
+	neuronsCountsInHiddenLayer.push_back(100);
+
+	double learningRate = 0.001;
+	double requiredAccuracy = 0.70;
+
+	CNeuralNetwork nnObj(posData, negData, neuronsCountsInHiddenLayer, learningRate, requiredAccuracy);
+	nnObj.StartTraining();
+
+	vector<double> testData;//fill
+	//after loading testData
+
+	nnObj.SetInputLayerData(testData);
+	nnObj.PropagateForwardFrom(1);
+
+	double output = nnObj.GetOutput();
+
+	cout << "Output = " << output << endl;
 
 	system("pause");
 	return 0;
